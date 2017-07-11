abc1167364eda23a228fa0fea50d4d96cf0faeff
Willian Teophilo <wteophilo@gmail.com>
2017-07-10 21:02:28 -0300

    Capitulo 5: Controller Order Limit

--- lojadecamisetas/app/controllers/produtos_controller.rb
+++ lojadecamisetas/app/controllers/produtos_controller.rb
@@ -1,2 +1,6 @@
 class ProdutosController < ApplicationController
+    def index
+        @produtos_preco = Produto.order(:preco).limit 3
+        @produtos_nome = Produto.order(:nome).limit 2 
+    end
 end
