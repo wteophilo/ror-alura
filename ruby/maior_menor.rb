1f1229abae74b9a8b43ef6bf2c46fdfed11661a9
Willian Teophilo <wteophilo@gmail.com>
2017-08-24 22:09:17 -0300

    Ruby 1: Capitulo 2 - Condicionais, laços, programação imperativa.

--- ruby/maior_menor.rb
+++ ruby/maior_menor.rb
@@ -7,8 +7,23 @@ puts "Escolhendo um número secreto entre 0 e 200..."
 numero_secreto = 175
 puts "Escolhido... que tal adivinhar hoje nosso número secreto?"
 puts "\n \n \n \n"
-puts "Tentativa 1"
-puts "Entre com o numero"
-chute = gets
-puts "Será que acertou? Você chutou " + chute
-puts numero_secreto == chute.to_i
+limite_de_tentativas = 5
+for tentativa in 1..limite_de_tentativas
+  puts "Tentativa "+ tentativa.to_s + " de " +limite_de_tentativas.to_s
+  puts "Entre com o numero"
+  chute = gets
+  puts "Será que acertou? Você chutou " + chute
+  acertou = numero_secreto == chute.to_i
+  if acertou
+    puts "Acertou!"
+    break
+  else
+    puts "Errou!"
+    maior = numero_secreto > chute.to_i
+    if maior
+      puts "Número secreto é maior"
+    else
+      puts "Número secreto é menor"
+    end
+  end
+end
