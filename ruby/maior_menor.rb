eff88998f19dffcf392d988782f72be930121858
Willian Teophilo <wteophilo@gmail.com>
2017-08-26 20:17:31 -0300

    Ruby 1: Capitulo 03: O poder da extração de código

--- ruby/maior_menor.rb
+++ ruby/maior_menor.rb
@@ -1,29 +1,55 @@
-puts "Bem vindo ao jogo da adivinhação"
-puts "Qual é o seu nome?"
-nome = gets
-puts "\n \n \n \n"
-puts "Começaremos o jogo para você, " + nome
-puts "Escolhendo um número secreto entre 0 e 200..."
-numero_secreto = 175
-puts "Escolhido... que tal adivinhar hoje nosso número secreto?"
-puts "\n \n \n \n"
-limite_de_tentativas = 5
-for tentativa in 1..limite_de_tentativas
+def pula_linha(quantidade)
+  for linha in 1..quantidade
+    puts "\n"
+  end
+end
+
+def da_boas_vindas
+  puts "Bem vindo ao jogo da adivinhação"
+  puts "Qual é o seu nome?"
+  nome = gets
+  pula_linha(4)
+  puts "Começaremos o jogo para você, " + nome
+end
+
+def sorteia_numero_secreto
+  puts "Escolhendo um número secreto entre 0 e 200..."
+  numero_secreto = 175
+  puts "Escolhido... que tal adivinhar hoje nosso número secreto?"
+  pula_linha(4)
+  numero_secreto
+end
+
+def pede_um_numero(tentativa,limite_de_tentativas)
   puts "Tentativa "+ tentativa.to_s + " de " +limite_de_tentativas.to_s
   puts "Entre com o numero"
   chute = gets
   puts "Será que acertou? Você chutou " + chute
-  acertou = numero_secreto == chute.to_i
+  chute.to_i
+end
+
+def verificar_se_acertou(numero_secreto,chute)
+  acertou = numero_secreto == chute
   if acertou
     puts "Acertou!"
-    break
+    return true
+  end
+  puts "Que pena você errou!"
+  maior = numero_secreto > chute.to_i
+  if maior
+    puts "Número secreto é maior"
   else
-    puts "Errou!"
-    maior = numero_secreto > chute.to_i
-    if maior
-      puts "Número secreto é maior"
-    else
-      puts "Número secreto é menor"
-    end
+    puts "Número secreto é menor"
+  end
+  false
+end
+
+da_boas_vindas
+numero_secreto = sorteia_numero_secreto
+limite_de_tentativas = 5
+for tentativa in 1..limite_de_tentativas
+  chute = pede_um_numero tentativa,limite_de_tentativas
+  if verificar_se_acertou numero_secreto,chute
+    break
   end
 end
