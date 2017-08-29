2731e735f051559929b77aa6f08ced61cfb7504b
Willian Teophilo <willian@vdmax3d.com.br>
2017-08-29 10:53:11 -0300

    Ruby 1: Aula 6- Case when,while,loop do

--- ruby/maior_menor.rb
+++ ruby/maior_menor.rb
@@ -5,16 +5,67 @@ def pula_linha(quantidade)
 end
 
 def da_boas_vindas
-  puts "Bem vindo ao jogo da adivinhação"
-  puts "Qual é o seu nome?"
-  nome = gets.strip
-  pula_linha(4)
-  puts "Começaremos o jogo para você, #{nome}"
+    puts
+    puts "        P  /_\  P                              "
+    puts "       /_\_|_|_/_\                             "
+    puts "   n_n | ||. .|| | n_n         Bem vindo ao    "
+    puts "   |_|_|nnnn nnnn|_|_|     Jogo de Adivinhação!"
+    puts "  |' '  |  |_|  |'  ' |                        "
+    puts "  |_____| ' _ ' |_____|                        "
+    puts "        \__|_|__/                              "
+    puts
+    puts "Qual é o seu nome?"
+    nome = gets.strip
+    puts "\n\n\n\n\n\n"
+    puts "Começaremos o jogo para você, #{nome}"
+    nome
 end
 
-def sorteia_numero_secreto
-  puts "Escolhendo um número secreto entre 0 e 200..."
-  numero_secreto = rand(200);
+def ganhou
+    puts
+    puts "             OOOOOOOOOOO               "
+    puts "         OOOOOOOOOOOOOOOOOOO           "
+    puts "      OOOOOO  OOOOOOOOO  OOOOOO        "
+    puts "    OOOOOO      OOOOO      OOOOOO      "
+    puts "  OOOOOOOO  #   OOOOO  #   OOOOOOOO    "
+    puts " OOOOOOOOOO    OOOOOOO    OOOOOOOOOO   "
+    puts "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO  "
+    puts "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO  "
+    puts "OOOO  OOOOOOOOOOOOOOOOOOOOOOOOO  OOOO  "
+    puts " OOOO  OOOOOOOOOOOOOOOOOOOOOOO  OOOO   "
+    puts "  OOOO   OOOOOOOOOOOOOOOOOOOO  OOOO    "
+    puts "    OOOOO   OOOOOOOOOOOOOOO   OOOO     "
+    puts "      OOOOOO   OOOOOOOOO   OOOOOO      "
+    puts "         OOOOOO         OOOOOO         "
+    puts "             OOOOOOOOOOOO              "
+    puts
+    puts "               Acertou!                "
+    puts
+end
+
+def pede_dificuldade
+  puts "Qual o nível de dificuldade?"
+  puts "(1)Muito Fácil (2)Fácil (3)Normal (4)Difícil (5)Impossível"
+  puts "Escolha: "
+  dificuldade = gets.to_i
+end
+
+def sorteia_numero_secreto(dificuldade)
+  case dificuldade
+    when 1
+      maximo = 30
+    when 2
+      maximo = 60
+    when 3
+      maximo = 100
+    when 4
+      maximo = 150
+    else
+      maximo = 200
+  end
+
+  puts "Escolhendo um número secreto entre 1 e #{maximo}..."
+  numero_secreto = rand(maximo)
   puts "Escolhido... que tal adivinhar hoje nosso número secreto?"
   pula_linha(4)
   numero_secreto
@@ -32,7 +83,7 @@ end
 def verificar_se_acertou(numero_secreto,chute,chutes)
   acertou = numero_secreto == chute
   if acertou
-    puts "Acertou!"
+    ganhou
     return true
   end
   puts "Que pena você errou!"
@@ -45,20 +96,40 @@ def verificar_se_acertou(numero_secreto,chute,chutes)
   false
 end
 
-da_boas_vindas
-numero_secreto = sorteia_numero_secreto
-chutes=[]
-limite_de_tentativas = 5
-pontos_ate_agora = 1000
-for tentativa in 1..limite_de_tentativas
-  chute = pede_um_numero chutes,tentativa,limite_de_tentativas
-  chutes<<chute
+def joga (nome,dificuldade)
+  numero_secreto = sorteia_numero_secreto dificuldade
+  chutes=[]
+  limite_de_tentativas = 5
+  pontos_ate_agora = 1000
+  for tentativa in 1..limite_de_tentativas
+    chute = pede_um_numero chutes,tentativa,limite_de_tentativas
+    chutes<<chute
 
-  pontos_a_perder = (chute-numero_secreto).abs/2.0
-  pontos_ate_agora -= pontos_a_perder
+    if nome.upcase == "WIL"
+        ganhou
+        break
+    end
+    pontos_a_perder = (chute-numero_secreto).abs/2.0
+    pontos_ate_agora -= pontos_a_perder
 
-  if verificar_se_acertou numero_secreto,chute,chutes
+    if verificar_se_acertou numero_secreto,chute,chutes
+      break
+    end
+  end
+  puts "Você ganhou #{pontos_ate_agora} pontos."
+end
+
+def nao_quero_jogar?
+    puts "Deseja jogar novamente? (S/N)"
+    continua = gets.strip
+    nao_quero_jogar = continua.upcase == "N"
+end
+
+nome = da_boas_vindas
+dificuldade = pede_dificuldade
+loop do
+  joga nome,dificuldade
+  if nao_quero_jogar?
     break
   end
 end
-puts "Você ganhou #{pontos_ate_agora} pontos."
