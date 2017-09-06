29fae86e83f965d3a2978815c0c6a5c10bbd671c
Willian Teophilo <wteophilo@gmail.com>
2017-09-06 07:22:23 -0300

    Ruby 3: Aula 2:  Movimentações do fantasmas e do herói e posições.

--- ruby/fogefoge/fogefoge.rb
+++ ruby/fogefoge/fogefoge.rb
@@ -20,17 +20,16 @@ end
 
 def calcula_nova_posicao(heroi,direcao)
   heroi = heroi.dup
-  case direcao
-    when "W"
-      heroi[0]-=1
-    when "S"
-      heroi[0]+=1
-    when "A"
-      heroi[1]-=1
-    when "D"
-      heroi[1]+=1
-    end
-    heroi
+  movimentos = {
+    "W" => [-1,0],
+    "S" => [+1,0],
+    "A" => [0,-1],
+    "D" => [0,+1]
+  }
+  movimento = movimentos[direcao]
+  heroi[0] += movimento[0]
+  heroi[1] += movimento[1]
+  heroi
 end
 
 
@@ -44,15 +43,37 @@ def posicao_valida?(mapa,posicao)
     fora_do_limite
     return false
   end
-  if mapa[posicao[0]][posicao[1]] =="X"
+
+  valor_atual = mapa[posicao[0]][posicao[1]]
+  if valor_atual == "X" || valor_atual == "F"
    bate_na_parede
    return false
   end
   true
 end
 
+def move_fantasma(mapa,linha,coluna)
+  posicao = [linha,coluna +1]
+  if posicao_valida? mapa, posicao
+    mapa[linha][coluna] = " "
+    mapa[posicao[0]][posicao[1]] = "F"
+  end
+end
+
+def move_fantasmas(mapa)
+  caractere_do_fantasma = "F"
+  mapa.each_with_index do |linha_atual,linha|
+    linha_atual.chars.each_with_index do |caractere_atual,coluna|
+      eh_fantasma = caractere_atual  == caractere_do_fantasma
+      if eh_fantasma
+        move_fantasma mapa,linha,coluna
+      end
+    end
+  end
+end
+
 def joga(nome)
-  mapa = le_arquivo 1
+  mapa = le_arquivo 2
   caractere_do_heroi = "H"
   while true
     desenha_mapa mapa
@@ -65,7 +86,9 @@ def joga(nome)
       next
     end
     mapa[heroi[0]][heroi[1]] = " "
-    mapa[nova_posicao[0]][nova_posicao[1]] = "H"
+    mapa[nova_posicao[0]][nova_posicao[1]] = caractere_do_heroi
+
+    move_fantasmas mapa
   end
 end
 
